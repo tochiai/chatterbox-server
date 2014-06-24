e182021bfb24c7223917bc117b2765b5d519d1a5
Hack Reactor Students <accounts@hackreactor.com>
2014-06-24 15:27:43 -0700

    Set process.env variables in server.js

--- server/basic-server.js
+++ server/basic-server.js
@@ -7,12 +7,12 @@ var handleRequest = require('./request-handler.js')
  * standard port for HTTP servers is port 80, but that port is
  * normally already claimed by another server and/or not accessible
  * so we'll use a higher port number that is not likely to be taken: */
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 /* For now, since you're running this server on your local machine,
  * we'll have it listen on the IP address 127.0.0.1, which is a
  * special address that always refers to localhost. */
-var ip = "127.0.0.1";
+var ip = process.env.HOST || "127.0.0.1";
 
 
 
